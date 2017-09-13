a9dd04d1b5d2f86951723c69567362ac6501cf36
edgabaldi <edgabaldi@gmail.com>
2017-09-13 14:02:31 -0300

    using wkhtmltopdf standalone test

--- report/management/commands/report.py
+++ report/management/commands/report.py
@@ -1,6 +1,24 @@
+#coding:utf-8
+
 from django.core.management.base import BaseCommand
+
+from wkhtmltopdf.utils import render_pdf_from_template
+
+from django.template.loader import get_template
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write('Hello World')
+
+        title =  'Hello World fú'
+
+        template = get_template('report/report.html')
+
+        pdf_content = render_pdf_from_template(template,
+            header_template=None,
+            footer_template=None,
+            context={'foo': title}
+        )
+
+        with open('arquivo.pdf', 'w') as arquivo:
+            arquivo.write(pdf_content)
